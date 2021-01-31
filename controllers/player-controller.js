974661a65441983d903536c08f8bf2f4e4f5c7fc
Coaxial <thecoaxial@gmail.com>
2021-01-31 16:05:54 -0500

    created user model and api routes

---
+++ controllers/player-controller.js
@@ -0,0 +1,50 @@
+var db = require("../models");
+
+//routes 
+module.exports = function(app){
+    app.get("/api/players", function(req, res){
+       db.Player.findAll({}).then(function(bdPlyer){
+            res.json(bdPlyer)
+        })
+    });
+    app.get("/api/players/:id", function(req, res){
+        db.Player.findOne({
+            where: {
+                id: req.params.id
+            },
+        }).then((dbPlayer) => res.json(dbPlayer));
+    });
+
+
+    app.put('/api/players', (req, res) => {
+        db.Player.update(req.body, {
+           where :{
+               id: req.body.id
+           },
+        }).then((dbPlayer) => res.json(dbPlayer));
+      });
+    
+app.post("/api/players", function(req, res){
+    db.Player.create({
+    name: req.body.name,
+    position: req.body.position
+    }).then( dbPlayer =>{
+     res.json(dbPlayer)
+    console.log(req.body)
+
+    })
+})
+app.delete("/api/players/:id", function(req,res){
+    db.Player.destroy({
+        where : {
+            id : req.params.id
+        }
+    }).then((dbPlayer) => res.json(dbPlayer));
+})
+}
+
+
+    
+    
+
+    
