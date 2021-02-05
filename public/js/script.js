b66c9fd03ba4ba0242be5781dabf98f65234f5db
lbmann <l.bmann@yahoo.com>
2021-02-04 22:46:30 -0500

    updating handlebars, css, script to have delete front end technology

--- public/js/script.js
+++ public/js/script.js
@@ -1,118 +1,103 @@
 
-// function to run as soon as the user lands on the player/team page
 
-    /* 
-        this function needs to: take all of the player li's filter into benched and match any with positions to their correct position on the field, possibly finding another solution for updatePosition, right now it is allowing text content to fill the void of having a "drop zone" and I am not sure this is the best approach.
-    
-    */
-
-    $(document).ready(function() {
-        // $("#signIn").show();
-        updatePositions();
-        // positionPlayers();
+$(document).ready(function() {
+      updatePositions();
+      positionPlayers();
     });
 
-  var updatePositions = function() {
+    var updatePositions = function() {
 
-      var positionLists = document.querySelectorAll(".container");
-      
-      console.log("running update positions");
+        var positionLists = document.querySelectorAll(".container");
 
-      for (i=0; i<positionLists.length; i++) {
+        for (i=0; i<positionLists.length; i++) {
 
-        console.log("in the for the loop");
+          if (positionLists[i].childElementCount == 0) {
 
-        if (positionLists[i].childElementCount == 0) {
+            var positionName = positionLists[i].getAttribute("data-position");
 
-          console.log("in the if statement");
-
-          var positionName = positionLists[i].getAttribute("data-position");
-
-          console.log(positionName);
-
-          positionLists[i].textContent = positionName;
-
+            positionLists[i].textContent = positionName;
+          }
         }
+
       }
 
-    }
 
+      var positionPlayers = function() {
 
-    /* position players function does not work yet -- commenting out in dev branch until functional */
+        var initialJoin = document.getElementById("benched");
 
-    // var positionPlayers = function() {
-    //     var initialJoin = document.getElementById("benched");
-    //     var players = initialJoin.getElementsByTagName("li");
+        var players = initialJoin.querySelectorAll("li");
 
-    //     for (i=0; i<players.length; i++) {
+        for (i=0; i<players.length; i++) {
 
-    //         if (players[i].getAttribute("data-position") !== "benched") {
+                var position = players[i].getAttribute("data-position");
 
-    //             var position = players[i].getAttribute("data-position");
+                var positionOptions = document.querySelectorAll(".container");
 
-    //             var positionOptions = document.querySelectorAll(".container");
+                for (p=0; p<positionOptions.length; p++) {
 
-    //             for (p=0; p<positionOptions.length; p++) {
-    //                 if (positionOptions[i].getAttribute("data-position") == position && positionOptions[i].childElementCount < 1) {
-    //                     // player[i].appendTo(positionOptions[i]);
+                    if (positionOptions[p].getAttribute("data-position") == position && positionOptions[p].childElementCount == 0) {
 
-    //                     positionOptions[i].append(players[i]);
-    //                 }
+                        positionOptions[p].append(players[i]);
+                    }
 
-    //             }
+              }
+          }
+      }
 
-    //         }
-    //     }
-    // }
+    $(".addPlayerForm").on("submit", event => {
 
+      event.preventDefault();
 
-  $(".addPlayerForm").on("submit", event => {
+      var newPlayerName = $(".addPlayerForm #newPlayerName").textContent;
 
-    event.preventDefault();
+      $.ajax("/api/playerRoster", {
 
-    var newPlayerName = $(".addPlayerForm #newPlayerName").textContent;
+          type: "POST",
 
-    $.ajax("/api/playerRoster", {
+          data: newPlayerName
 
-        type: "POST",
+          }).then(() => {
 
-        data: newPlayerName
+          location.reload();
 
-        }).then(() => {
+        });
 
-        location.reload();
+    });
 
-      });
+    var drake = dragula({
+      isContainer: function(el) {
+        return el.classList.contains('container') || el.classList.contains('container-benched');
+      },
+      revertOnSpill: true
+    });
 
-  });
+    drake.on('drop', function(el, target, source, sibling) {
+        
+      // only one player per position
+      if (target.getAttribute("data-position") !== "benched" && target.childElementCount > 1) {
+        alert("You have already placed a player in this position!");
+        drake.cancel();
+      }
 
-  var drake = dragula({
-    isContainer: function(el) {
-      return el.classList.contains('container') || el.classList.contains('container-benched');
-    },
-    revertOnSpill: true
-  });
-
-  drake.on('drop', function(el, target, source, sibling) {
-
-
-    console.log("target= " + target.childElementCount)
+      else {
+        var newPosition = target.getAttribute("data-position");
+        el.setAttribute("data-position", newPosition);
+        // target.textContent = "";
+      }
       
-    // only one player per position
-    if (target.getAttribute("data-position") !== "benched" && target.childElementCount > 1) {
-      alert("You have already placed a player in this position!");
-      drake.cancel();
-    }
+    });
 
-    else {
-      var newPosition = target.getAttribute("data-position");
-      el.setAttribute("data-position", newPosition);
-      // target.textContent = "";
-      console.log("success!");
-    }
-    
-  });
+    drake.on('cancel', function(el, container, source) {
+      console.log("cancelled!");
+    });
 
-  drake.on('cancel', function(el, container, source) {
-    console.log("cancelled!");
-  });
+
+    $(".delete").on("click", e => {
+        console.log(e.target);
+        var target = e.target;
+        var li = target.closest(".player");
+        console.log(li);
+        li.remove();
+
+    });
