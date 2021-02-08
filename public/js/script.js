a942922dbaa63d4a47ec9396ea1f16c2179afa50
lbmann <l.bmann@yahoo.com>
2021-02-08 05:21:47 -0500

    adding very important prevent propagation changes to keep events from doubling because of page reloads

--- public/js/script.js
+++ public/js/script.js
@@ -5,24 +5,24 @@
 
 // nav scripts -- changing content and active tab color
 
-  function openOption(tabs) {
-    var i;
-    var x = document.getElementsByClassName("tab");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById(tabs).style.display = "block";
+function openOption(tabs) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  document.getElementById(tabs).style.display = "block";
+}
 
-  $(".bar-item").on("click", function(e) {
+$(".bar-item").on("click", function(e) {
 
-    document.querySelectorAll(".bar-item").forEach(function(element) {
-        element.classList.remove("activeTab");
-    });
-
-      e.currentTarget.classList += " activeTab";
-
+  document.querySelectorAll(".bar-item").forEach(function(element) {
+      element.classList.remove("activeTab");
   });
+
+    e.currentTarget.classList += " activeTab";
+
+});
 
 /* -------------------------------------------- */
 /* -------- SCRIPTS FOR INDEX.HANDLEBARS ------ */
@@ -32,21 +32,33 @@
 
   var positionPlayers = function() {
 
-    var initialJoin = document.getElementById("benched");
+    console.log("running position players");
 
-    var players = initialJoin.querySelectorAll("li");
+    var players = $("#benched").find("li");
+
+    console.log(players);
+
+    console.log("players length: " + players.length)
 
     for (i=0; i<players.length; i++) {
 
+            console.log("getting players");
+
             var position = players[i].getAttribute("data-position");
 
+            console.log(position);
+
             var positionOptions = document.querySelectorAll(".container");
+
+            console.log(positionOptions);
 
             for (p=0; p<positionOptions.length; p++) {
 
                 if (positionOptions[p].getAttribute("data-position") == position && positionOptions[p].childElementCount == 0) {
 
                     positionOptions[p].append(players[i]);
+
+                    console.log("appending players to positions");
                 }
 
           }
@@ -54,184 +66,204 @@
   }
 
 
-    // handles form submission of the "add player" form and it's ajax call
+  // handles form submission of the "add player" form and it's ajax call
 
-    $(".addPlayerForm").on("submit", event => {
+  $(".addPlayerForm").on("submit", event => {
 
-      event.preventDefault();
+    event.preventDefault();
 
-      var playerLimit = document.querySelectorAll(".container li, .container-benched li");
+    event.stopImmediatePropagation();
 
-      if (playerLimit.length >= 11) {
+    var playerLimit = document.querySelectorAll(".container li, .container-benched li");
 
-        alert("You already have the maximum amount of players! Please delete a player to add another.");
-      
-      }
+    if (playerLimit.length >= 11) {
 
-      else {
-      
-        // var newPlayerName = $(".addPlayerForm #newPlayerName").textContent;
+      alert("You already have the maximum amount of players! Please delete a player to add another.");
     
-        var playerData = {
-        
-          name: document.getElementById("newPlayerName").value
-        
-        }
+    }
 
-        console.log(playerData.name);
-
-        $.ajax("/api/players", {
-
-            type: "POST",
-
-            data: playerData,
-
-            success: function(req, err) { console.log("A new player has been created!"); },
-
-            error: function(req, err) { console.log("Your player failed to post with: " + err); }
-
-            }).then(() => {
-
-            // updatePlayers();
-
-            location.reload();
-
-          });
-
+    else {
+    
+      // var newPlayerName = $(".addPlayerForm #newPlayerName").textContent;
+  
+      var playerData = {
+      
+        name: document.getElementById("newPlayerName").value
+      
       }
 
-    });
+      console.log(playerData.name);
 
+      $.ajax("/api/players", {
 
-    // handles ajax call and frontend handling of the "delete" button for a player
+          type: "POST",
 
-    $(".delete").on("click", e => {
-        var target = e.currentTarget;
-        console.log(target);
-        var li = target.closest(".player");
-        console.log(li);
-        var id = li.getAttribute("data-id");
-        var url = "/api/players/" + id
+          data: playerData,
 
-        $.ajax(url, {
+          success: function(req, err) { console.log("A new player has been created!"); },
 
-            type: "DELETE",
+          error: function(req, err) { console.log("Your player failed to post with: " + err); }
 
-            error: function(req, err) { console.log("Your player failed to delete with err: " + err); }
+          }).then(() => {
 
-            }).then(() => {
+          location.reload();
 
-            li.remove();
+          // window.location.reload();
 
-            location.reload();
+        }).then(() => {
+
+          location.reload();
+
+          positionPlayers();
 
         });
 
-      console.log(li);
+      
 
-      // drake.remove();
+    }
 
   });
 
 
-  // handles the user input, ajax update call, and the page reload for updating a single player
+  // handles ajax call and frontend handling of the "delete" button for a player
 
-  $(".update").on("click", e => {
-      console.log(e.currentTarget);
+  $(".delete").on("click", e => {
+      e.preventDefault();
+      e.stopImmediatePropagation();
       var target = e.currentTarget;
+      console.log(target);
       var li = target.closest(".player");
-      var name = li.getAttribute("data-name");
+      console.log(li);
       var id = li.getAttribute("data-id");
-      console.log(id);
       var url = "/api/players/" + id
-      console.log(name);
 
-      var newName = window.prompt("Update the Player name:", name);
+      $.ajax(url, {
 
-      var playerData = {
-        name: newName
-      }
+          type: "DELETE",
 
-    console.log(newName);
+          error: function(req, err) { console.log("Your player failed to delete with err: " + err); }
 
-      if (newName) {
+          }).then(() => {
 
-          $.ajax(url, {
+          li.remove();
 
-            type: "PUT",
+          location.reload();
 
-            data: playerData,
+      });
 
-            error: function(req, err) { console.log("Your player failed to update with a new name: " + err); }
+    console.log(li);
 
-            }).then(() => {
+    // drake.remove();
 
-            // updatePlayers();
+});
 
-            location.reload();
 
-          });
-      }
-    
+// handles the user input, ajax update call, and the page reload for updating a single player
 
-    });
+$(".update").on("click", e => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    console.log(e.currentTarget);
+    var target = e.currentTarget;
+    var li = target.closest(".player");
+    var name = li.getAttribute("data-name");
+    var id = li.getAttribute("data-id");
+    console.log(id);
+    var url = "/api/players/" + id
+    console.log(name);
 
-    /* --------------------- */
-    /* -- dragula scripts -- */
-    /* --------------------- */
+    var newName = window.prompt("Update the Player name:", name);
 
-    // sets up where the li can be dragged to, and to revert to last position upon spill
+    var playerData = {
+      name: newName
+    }
 
-    var drake = dragula({
-      isContainer: function(el) {
-        return el.classList.contains('container') || el.classList.contains('container-benched');
-      },
-      revertOnSpill: true
-    });
+    console.log(playerData.name);
 
-    // limit the amount of players that can be in a position (1), besides benched 
+    if (newName) {
 
-    drake.on('drop', function(el, target, source, sibling) {
-        
-      // only one player per position
-      if (target.getAttribute("data-position") !== "benched" && target.childElementCount > 1) {
-        alert("You have already placed a player in this position!");
-        drake.cancel();
-      }
-
-      else {
-        var newPosition = target.getAttribute("data-position");
-        el.setAttribute("data-position", newPosition);
-        var id = el.getAttribute("data-id");
-        console.log(id);
-        var url = "/api/players/" + id
-
-        var playerData = {
-          position: newPosition
-        }
+      console.log("adding a new name is happening.");
 
         $.ajax(url, {
 
-            type: "PUT",
+          type: "PUT",
 
-            data: playerData,
+          data: playerData,
 
-            error: function(req, err) { console.log("Your player failed to update with a new position" + err); }
+          success: function(req, err) { console.log("New name is successful!"); },
 
-            }).then(() => {
+          error: function(req, err) { console.log("Your player failed to update with a new name: " + err); }
 
-            // updatePlayers();
+          }).then(() => {
 
-            // location.reload();
+          console.log("made it to /then/ statement.");
 
-          });
-      }
+          // updatePlayers();
+
+          location.reload();
+
+        });
+    }
+  
+
+  });
+
+  /* --------------------- */
+  /* -- dragula scripts -- */
+  /* --------------------- */
+
+  // sets up where the li can be dragged to, and to revert to last position upon spill
+
+  var drake = dragula({
+    isContainer: function(el) {
+      return el.classList.contains('container') || el.classList.contains('container-benched');
+    },
+    revertOnSpill: true
+  });
+
+  // limit the amount of players that can be in a position (1), besides benched 
+
+  drake.on('drop', function(el, target, source, sibling) {
       
-    });
+    // only one player per position
+    if (target.getAttribute("data-position") !== "benched" && target.childElementCount > 1) {
+      alert("You have already placed a player in this position!");
+      drake.cancel();
+    }
 
-    drake.on('cancel', function(el, container, source) {
-      console.log("cancelled!");
-    });
+    else {
+      var newPosition = target.getAttribute("data-position");
+      el.setAttribute("data-position", newPosition);
+      var id = el.getAttribute("data-id");
+      console.log(id);
+      var url = "/api/players/" + id
 
+      var playerData = {
+        position: newPosition
+      }
 
+      $.ajax(url, {
+
+          type: "PUT",
+
+          data: playerData,
+
+          error: function(req, err) { console.log("Your player failed to update with a new position" + err); }
+
+          }).then(() => {
+
+          // updatePlayers();
+
+          // location.reload();
+
+        });
+    }
     
+  });
+
+  drake.on('cancel', function(el, container, source) {
+    console.log("cancelled!");
+  });
+
+
+  
